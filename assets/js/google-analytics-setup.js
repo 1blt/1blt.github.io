c9a5bc49721a77f42c43d4f26002033127a6919f
1blt <alan.wang@huntridgelabs.com>
2026-02-10 15:03:21 +0000

    Deploying to gh-pages from @ 1blt/1blt.github.io@f80e5695539c40af72416a2a3f52a819c148e2d9 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -1,2 +1,6 @@
-window.dataLayer=window.dataLayer||[];function gtag(){window.dataLayer.push(arguments);}
-gtag("js",new Date());gtag("config","G-8K53Q9C277");
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-8K53Q9C277");
